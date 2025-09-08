460feb6a00f97b554956ba719b297f84c8c9ddd8
Radar-Subastas <radarsubastas@gmail.com>
2025-09-08 12:37:49 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Radar Subastas
+
+Proyecto inicial con **Next.js** desplegado en **Cloudflare Pages**.
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>🚀 Radar Subastas</h1>
+      <p>Bienvenido a tu primera app desplegada en Cloudflare Pages con Next.js</p>
+    </div>
+  );
+}
